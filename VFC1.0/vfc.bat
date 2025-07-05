863f97d52dcf040c0dff869bb156e85eb1f63071
Lopez, Luis R. <luis.lopez-3@leidos.com>
2025-07-04 23:22:49 -0500

    fixed timer on vfc.bat to avoid double file opening

--- VFC1.0/vfc.bat
+++ VFC1.0/vfc.bat
@@ -1,26 +1,31 @@
 
 
 echo off
-echo ---  [%1]  
-cd 
+echo 		FILE: [%1]  
+
 
 IF "%~1"=="" (
-    echo file: [%1]--- No file given, launching VFC2000
+    echo 		No file given, launching VFC2000
     start vfc2000
 ) ELSE (
 
     if /I not "%~x1"==".vfc" (
         IF NOT EXIST "%~1.vfc" (
-            echo --- PARSING %1
-            echo ---  
-            C++parse.bat %~1
-            echo --- PARSED %1 COMPLETED
+            echo 	PARSING %1
+            echo 	---  
+            C++parse.bat %~1 
+            echo 		--- PARSED %1 COMPLETED
+	    timeout 1
+	    vfc2000 %1.vfc -Goto %2
+	    exit
         )
-    	echo --- VFC FILE %1 READY TO GOTO %2
+    	echo 		--- VFC FILE %1 OPEN %2
+        echo 		--- done 
     	start vfc2000 %1.vfc -Goto %2
-	rem timeout 3
+	rem timeout 1
     ) ELSE (
-        echo --- EXISTING VFC FILE %1 -Goto %2
+        echo 		--- EXISTING VFC FILE %1 -Goto %2
+        echo 		--- done 
     	start vfc2000 %1 -Goto %2    
     )
 )
