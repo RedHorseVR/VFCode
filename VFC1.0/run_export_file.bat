1fbfa2f0594338025c4a89fdf452f6a1fc87020a
Luis Lopez <luis.r.lopez@outlook.com>
2026-04-26 03:07:34 -0500

    ...

---
+++ VFC1.0/run_export_file.bat
@@ -0,0 +1,27 @@
+
+echo off
+echo DIR------------------
+echo %CD%
+echo FILE-----------------
+for %%a in ("%1") do set FILE=%%~nxa
+echo file: %FILE%
+FOR %%f IN (%FILE%) DO set EXPORT=%%~nf
+echo export: %EXPORT%
+
+rem cmd / k 
+
+cd %2
+
+echo %EXPORT% | findstr /I ".py" >nul
+if %errorlevel% equ 0 (
+    echo The file has a .py extension. Using Python
+    start python %CD%/%EXPORT%
+) else (
+    echo The file does not have a .py extension.
+)
+
+TIMEOUT 2
+
+pause
+
+EXIT
